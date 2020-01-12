557ddbeb7f95f679af567fa027edc8cbfa84c173
YishengSun <yisheng4@illinois.edu>
2020-01-12 11:44:56 -0600

    Updates

---
+++ precache-manifest.bd54e6e9357d98d344d5dff62124bbca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37491c8bf36d92b80cb578f0674a1a98",
+    "url": "/Pokemon-game/index.html"
+  },
+  {
+    "revision": "5dde0f771f9599d42429",
+    "url": "/Pokemon-game/static/css/main.50f843d9.chunk.css"
+  },
+  {
+    "revision": "03810c0c3bed72a9a7b5",
+    "url": "/Pokemon-game/static/js/2.4da8eeeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Pokemon-game/static/js/2.4da8eeeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dde0f771f9599d42429",
+    "url": "/Pokemon-game/static/js/main.a30ca946.chunk.js"
+  },
+  {
+    "revision": "9eb95f6ac4dc4b333dd4",
+    "url": "/Pokemon-game/static/js/runtime-main.48e320bb.js"
+  }
+]);
